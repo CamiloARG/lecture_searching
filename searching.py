694df1e57b904cc41e9aa3e0821c085b3ac7440d
Romero Gomez Camilo Adrian (262744) <262744@vutbr.cz>
2024-04-08 12:29:17 +0200

    ukolo

--- searching.py
+++ searching.py
@@ -12,10 +12,17 @@ def read_data(file_name, field):
     :return: (list, string),
     """
     file_path = os.path.join(cwd_path, file_name)
+    with open("sequential.json", mode="r") as data_file:
+        data = json.load(data_file)
 
+    if field in data.keys():
+        return data[field]
+    else:
+        return None
 
 def main():
-    pass
+    sequential_data = read_data("sequential.json", "unordered_numbers")
+    print(sequential_data)
 
 
 if __name__ == '__main__':

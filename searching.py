460a5618b1259df1322370326e79cd3c28ad4ff9
Romero Gomez Camilo Adrian (262744) <262744@vutbr.cz>
2024-04-08 13:28:30 +0200

    ukol

--- searching.py
+++ searching.py
@@ -1,4 +1,5 @@
 import os
+import json
 
 # get current working directory path
 cwd_path = os.getcwd()
@@ -37,10 +38,35 @@ def linear_search(sequence, number):
 
     return {"positions": positions, "count": len(positions)}
 
+def pattern_search(searched_sequence,searched_pattern):
+    """
+
+    :param searched_sequence:
+    :param searched_pattern:
+    :return:
+    """
+    with open("sequential.json", mode="r") as data_file:
+        data = json.load(data_file)
+        searched_sequence = data["dna_sequence"]
+
+    list_idx = []
+
+    for i, number in enumerate(searched_sequence):
+        if number in enumerate(searched_pattern):
+            list_idx.append(i)
+        else:
+            pass
+
+    return list_idx
+
+
 def main():
+
     sequential_data = read_data("sequential.json", "unordered_numbers")
+    searched_number = linear_search("sequential.json", 31)
+    searched_pattern = pattern_search("ATA", 1)
     print(sequential_data)
-
-
+    print(searched_number)
+    print(searched_pattern)
 if __name__ == '__main__':
     main()
